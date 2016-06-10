86b2c54fde8d999b77de0026dfefe8b149caac67
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-06-11 03:56:43 +0900

    add plugin

---
+++ plugin-hans-on-sample.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Plugin-hans-on-sample
+ * Version: 0.1-alpha
+ * Description: PLUGIN DESCRIPTION HERE
+ * Author: YOUR NAME HERE
+ * Author URI: YOUR SITE HERE
+ * Plugin URI: PLUGIN SITE HERE
+ * Text Domain: plugin-hans-on-sample
+ * Domain Path: /languages
+ * @package Plugin-hans-on-sample
+ */
